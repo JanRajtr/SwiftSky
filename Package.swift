5f59ea44811072ca08630618a7283c1870f267b9
Jan Rajtr <rajtrj@cngroup.dk>
2019-10-24 14:57:26 +0200

    - edited project structure to conform to swift-package-manager 5.1 - edited package manifest

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.1
 //
 //  Package.swift
 //  SwiftSky
@@ -10,5 +11,22 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSky",
-    exclude: ["Tests"]
+    
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftSky",
+            targets: ["SwiftSky"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftSky",
+            dependencies: [])
+    ]
 )
